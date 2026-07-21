2badb8acef785df8fd81f390c470b73718554b07
VYOM PATEL <vyom1912@gmail.com>
2026-07-21 16:32:39 +0530

    add some module for github host

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   // Required for SPA routing — serve index.html for any unmatched route
   preview: { port: 4173 },
-  server:  { historyApiFallback: true },
-})
+  server: { historyApiFallback: true },
+  base: "/Makewell-Agri-Equipments/",
+});
